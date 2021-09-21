2f7aa7ac1e7a90b80a3fb9535dedf2d7e4df33dc
NourEgy <inouregy@gmail.com>
2021-09-22 02:47:18 +0300

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// import pages
+import Home from './pages/Home'
+import About from './pages/About'
+import SingleCocktail from './pages/SingleCocktail'
+import Error from './pages/Error'
+// import components
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Navbar />
+
+      <Switch>
+        <Route exact path="/">
+            <Home />
+        </Route>
+
+
+        <Route exact path="/About">
+            <About />
+        </Route>
+
+          <Route path="/cocktail/:id">
+            <SingleCocktail />
+          </Route>
+
+        <Route exact path="*">
+            <Error />
+        </Route>
+
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import { Link , NavLink } from 'react-router-dom'
+import logo from '../logo.svg'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <div className="nav-center">
+            <Link  to="/">
+                 <img src={logo} alt="logo" className="logo" />
+            </Link >
+
+            <ul className="nav-links">
+                <li> <NavLink exact to="/"> Home </NavLink> </li>
+                <li> <NavLink to="/about"> About </NavLink> </li>
+            </ul>
+        </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
